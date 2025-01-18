7bdd823ec0bb9f7bdaf6b35769d8f9d06739d09c
geethalekshmi42 <glekshmi003@gmail.com>
2025-01-18 12:26:56 +0530

    Add files via upload

---
+++ OOPS.py
@@ -0,0 +1,42 @@
+# course_code = "EN150"
+# print(type(course_code))
+#
+# course_name = "BA Economics"
+# print(type(course_name))
+#
+# credit_hours = 30
+# print(type(credit_hours))
+
+# class ProtectedExample:
+#     def _init_(self, name):
+#         self._name = name
+#
+#     def _protected_method(self):
+#             print("calling the protected method from within the class:")
+#             self._protected_method()
+#
+# obj = ProtectedExample("Economics")
+#
+# print("Accessing protected variable directly:",obj._name)
+#
+# print("calling protected method directly:")
+# obj.public_method()
+#
+# class SubclassExample(ProtectedExample):
+#     def display_name(self):
+#         print(f"Accessing protected variable in subclass: {self._name}")
+#         print("calling protected method from subclass:")
+#         self._protected_method()
+#
+# sub_obj = SubclassExample("Mathematics")
+# sub_obj.display_name()
+
+# ElectiveCourse = "Mathematics"
+# print(type(ElectiveCourse))
+
+# greet="Hello"
+#
+# persons={
+#     "name":"Lechu",
+#     "Salary":20000
+# }
